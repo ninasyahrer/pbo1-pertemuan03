990394e67501a0a19cbb19701941d930b8ca30a4
ninasyahrer <ninasyahrer07@gmail.com>
2020-05-02 22:55:26 +0800

    Contoh If dengan condition false sehingga statement2 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,14 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        public static void main(String[] args) {
-            double totalBelanja = 25000;
-            double uangDiDompet = 53000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 53000;
 
-            if (uangDiDompet<totalBelanja){
-                System.out.println("Uang kurang, kurangi jajan anda");
-            } else {
-                System.out.println("Uang cukup, berelaanlah");
-            }
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        } else {
+            System.out.println("Uang cukup, berelaanlah");
         }
+    }
 }
