0a2ea9eb27204084e08fc119952b38728ed1fe63
ninasyahrer <ninasyahrer07@gmail.com>
2020-05-02 22:43:46 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Nina Syahrer";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.89;
+}
